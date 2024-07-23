b65cca4aacc763763ebfd033da29b284123ce630
vgotu99 <vgotu99@gmail.com>
2024-07-24 00:55:27 +0900

    Add files via upload

--- Section03/conditionals.js
+++ Section03/conditionals.js
@@ -25,18 +25,36 @@ else if (obj.name === 'Jason') {
 } else {
     console.log('You are not our member!')
 }
+//===========
 
 // && (AND 연산자) => 두 조건을 모두 만족하는 경우 true
-// || (OR 연산자) => 두 조건 중 하나라도 만족하는 경우 true
+if (obj.name === 'Peter' && obj.age === 25) {
+    console.log('Hi,' + obj.age +'years old' + obj.name)
+} else {
+    console.log('You are not our member!')
+}
 
+// || (OR 연산자) => 두 조건 중 하나라도 만족하는 경우 true
 if (obj.name === 'Peter' || obj.name === 'Jason') {
     console.log('Hi,' + obj.name)
 } else {
     console.log('You are not our member!')
 }
 
-if (obj.name === 'Peter' && obj.age === 25) {
-    console.log('Hi,' + obj.age +'years old' + obj.name)
-} else {
-    console.log('You are not our member!')
-}
+// ! (NOT 연산자) => 반대되는 값을 true or false로 출력해줌
+!undefined
+true
+!null
+true
+!0
+true
+!""
+true
+!NaN
+true
+![]
+false
+!1
+false
+
+
